32d51ce03d03ced7eab4cb9ef8d7f7489dea099b
MUHAMMAD ADE SULISTIANSYAH <atian8109@gmail.com>
2026-06-20 23:16:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,58 +8,105 @@ init();
 });
 
 function init(){
-trend();
-selector();
-insight();
+createTrend();
+createMap();
+createCountry();
+createRanking();
 }
 
-function trend(){
-const yearly={};
+/* TREND */
+function createTrend(){
+
+let yearly = {};
+
 data.forEach(d=>{
 if(!yearly[d.year]) yearly[d.year]=[];
 yearly[d.year].push(d.value);
 });
 
-const labels=Object.keys(yearly);
-const values=labels.map(y=>
+let labels = Object.keys(yearly);
+
+let values = labels.map(y =>
 yearly[y].reduce((a,b)=>a+b,0)/yearly[y].length
 );
 
 new Chart(document.getElementById("trendChart"),{
 type:"line",
 data:{
-labels:labels,
-datasets:[{label:"Sea Level",data:values,borderColor:"#4fc3f7"}]
+labels,
+datasets:[{
+label:"Sea Level Anomaly",
+data:values,
+borderColor:"#4fc3f7"
+}]
 }
 });
 }
 
-function selector(){
-const s=document.getElementById("countrySelect");
-const countries=[...new Set(data.map(d=>d.country))];
+/* MAP */
+function createMap(){
+
+let map = L.map('map').setView([-10,160],4);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+L.marker([-8.5,179]).addTo(map).bindPopup("Tuvalu");
+L.marker([-17.7,168]).addTo(map).bindPopup("Vanuatu");
+L.marker([-1.4,173]).addTo(map).bindPopup("Kiribati");
+}
+
+/* COUNTRY */
+function createCountry(){
+
+let select = document.getElementById("countrySelect");
+
+let countries = [...new Set(data.map(d=>d.country))];
+
 countries.forEach(c=>{
-const o=document.createElement("option");
-o.value=c;o.innerText=c;
-s.appendChild(o);
+let opt = document.createElement("option");
+opt.value=c;
+opt.innerText=c;
+select.appendChild(opt);
 });
 
-s.addEventListener("change",()=>render(s.value));
+select.onchange = ()=>render(select.value);
+
 render(countries[0]);
 }
 
 function render(country){
-const f=data.filter(d=>d.country===country);
+
+let filtered = data.filter(d=>d.country===country);
+
 new Chart(document.getElementById("countryChart"),{
 type:"line",
 data:{
-labels:f.map(d=>d.year),
-datasets:[{label:country,data:f.map(d=>d.value),borderColor:"#ffcc80"}]
+labels:filtered.map(d=>d.year),
+datasets:[{
+label:country,
+data:filtered.map(d=>d.value),
+borderColor:"#ffcc80"
+}]
 }
 });
 }
 
-function insight(){
-const avg=data.reduce((a,b)=>a+b.value,0)/data.length;
-document.getElementById("insightBox").innerHTML=
-"Average anomaly: "+avg.toFixed(3)+" m";
+/* RANKING */
+function createRanking(){
+
+let latest = {};
+
+data.forEach(d=>{
+if(!latest[d.country] || d.year > latest[d.country].year){
+latest[d.country] = d;
+}
+});
+
+let sorted = Object.values(latest)
+.sort((a,b)=>b.value-a.value);
+
+document.getElementById("ranking").innerHTML =
+sorted.map((d,i)=>
+`${i+1}. ${d.country} → ${d.value}`
+).join("<br>");
 }
